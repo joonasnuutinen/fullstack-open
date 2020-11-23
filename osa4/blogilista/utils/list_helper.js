ca2d9a19236c8ba5ead11656e182010248db37bb
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-23 10:18:30 +0200

    Do 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, b) => sum + b.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
