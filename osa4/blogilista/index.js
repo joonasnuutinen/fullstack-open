15c398dce609f19b3430e470c542bae83edd3803
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-23 09:22:45 +0200

    Modularize logger

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,4 +1,9 @@
+/*const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')*/
+
 require('dotenv').config()
+const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -39,5 +44,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
