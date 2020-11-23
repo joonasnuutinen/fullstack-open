0eb589e4bed25af2830ef9975590c1899a00d975
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-23 11:44:42 +0200

    Do 4.7

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -13,9 +13,12 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((b1, b2) => b2.likes > b1.likes ? b2 : b1)
 }
 
+const blogsByAuthor = (blogs) => {
+  return _.groupBy(blogs, 'author')
+}
+
 const mostBlogs = (blogs) => {
-  const blogsByAuthor = _.groupBy(blogs, 'author')
-  const countsByAuthor = _.map(blogsByAuthor, (blogs, author) => {
+  const countsByAuthor = _.map(blogsByAuthor(blogs), (blogs, author) => {
     return {
       author,
       blogs: blogs.length
@@ -24,9 +27,20 @@ const mostBlogs = (blogs) => {
   return _.maxBy(countsByAuthor, ab => ab.blogs)
 }
 
+const mostLikes = (blogs) => {
+  const likesByAuthor = _.map(blogsByAuthor(blogs), (blogs, author) => {
+    return {
+      author,
+      likes: _.sumBy(blogs, b => b.likes)
+    }
+  })
+  return _.maxBy(likesByAuthor, al => al.likes)
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes
 }
