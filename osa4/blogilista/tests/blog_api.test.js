c6521a30f8a87381345d8f3cec7dacb98533fdef
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-23 13:42:20 +0200

    Do 4.8

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Joonas',
+    url: 'https://autua.fi',
+    likes: 15,
+  },
+  {
+    title: 'Second blog',
+    author: 'Matti',
+    url: 'https://fullstackopen.com/osa4/backendin_testaaminen',
+    likes: 300,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
