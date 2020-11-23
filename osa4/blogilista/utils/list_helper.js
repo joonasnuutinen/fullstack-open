66e63455a1097b1752524079d28a3353c52f25ae
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-23 10:38:55 +0200

    Do 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -7,7 +7,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, b) => sum + b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((b1, b2) => b2.likes > b1.likes ? b2 : b1)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
