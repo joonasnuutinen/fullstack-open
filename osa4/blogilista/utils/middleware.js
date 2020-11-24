6fc691e2842ea682367b65386ee842d72afbf7ec
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-24 13:33:18 +0200

    Do 4.19

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -11,6 +11,10 @@ const errorHandler = (err, req, res, next) => {
     return res.status(400).send({ error: err.message })
   }
 
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' })
+  }
+
   next(err)
 }
 
