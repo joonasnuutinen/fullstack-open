00f3c22f844208149c15a407affe40e2af73bc13
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-24 13:19:06 +0200

    Do 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.errorHandler)
