248b5e69bc72a408a971cb1a6f02c52e78298535
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-24 13:39:21 +0200

    Do 4.20

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -18,6 +18,16 @@ const errorHandler = (err, req, res, next) => {
   next(err)
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    req.token = authorization.substring(7)
+  } else {
+    req.token = null
+  }
+  next()
+}
+
 module.exports = {
-  errorHandler
+  errorHandler, tokenExtractor
 }
