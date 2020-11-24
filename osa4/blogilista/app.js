b43223bb7ed61fdc2c2b93ad0ab39247eeed5c5e
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-24 11:23:45 +0200

    Do 4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 
@@ -12,6 +13,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.errorHandler)
---
+++ osa4/blogilista/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  name: String,
+  passwordHash: String,
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (doc, obj) => {
+    obj.id = obj._id.toString()
+    delete obj._id
+    delete obj.__v
+    delete obj.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
