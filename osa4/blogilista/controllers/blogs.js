9b7bc13fbe999ff7f5eb5524ec76414042e8f5fe
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-24 13:55:47 +0200

    Do 4.21

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -36,7 +36,22 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(204).end()
+  }
+
+  if (blog.user?.toString() !== decodedToken.id.toString()) {
+    return response.status(401).json({ error: 'user is not authorized to remove this blog' })
+  }
+
+  blog.remove()
   response.status(204).end()
 })
 
