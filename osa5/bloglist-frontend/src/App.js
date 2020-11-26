cfb451157bf2f7f551ae87cd86ef0e92175656c7
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-26 09:52:46 +0200

    Do 5.9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -13,13 +13,16 @@ const LoginForm = ({ username, setUsername, password, setPassword, onSubmit }) =
   </form>
 )
 
-const BlogList = ({ blogs, handleLike }) => (
-  <div>
-    {blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-    )}
-  </div>
-)
+const BlogList = ({ blogs, handleLike }) => {
+  const sortedBlogs = blogs.sort((b1, b2) => b2.likes - b1.likes)
+  return (
+    <div>
+      {sortedBlogs.map(blog =>
+        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+      )}
+    </div>
+  )
+}
 
 const ErrorMessage = ({ message }) => {
   if (!message) return null
