a3070f7dc792830ae2d9fdc18637306da31bb5b1
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-11-26 13:44:03 +0200

    Do 5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,13 +3,14 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-describe('<Blog /> component renders', () => {
-  const blog = {
-    title: 'test blog',
-    author: 'test author',
-    url: 'https://localhost:3000',
-    likes: 100,
-  }
+const blog = {
+  title: 'test blog',
+  author: 'test author',
+  url: 'https://localhost:3000',
+  likes: 100,
+}
+
+describe('<Blog /> component', () => {
   let component
 
   beforeEach(() => {
@@ -18,14 +19,14 @@ describe('<Blog /> component renders', () => {
     )
   })
 
-  test('only title and author by default', () => {
+  test('renders only title and author by default', () => {
     expect(component.container).toHaveTextContent(blog.title)
     expect(component.container).toHaveTextContent(blog.author)
     expect(component.container).not.toHaveTextContent(blog.url)
     expect(component.container).not.toHaveTextContent(blog.likes)
   })
 
-  test('after clicking the button, also url and likes', () => {
+  test('renders also url and likes after the button has been clicked', () => {
     const button = component.getByText('view')
     fireEvent.click(button)
 
@@ -33,5 +34,25 @@ describe('<Blog /> component renders', () => {
     expect(component.container).toHaveTextContent(blog.author)
     expect(component.container).toHaveTextContent(blog.url)
     expect(component.container).toHaveTextContent(blog.likes)
+  })
+})
+
+describe('clicking the like button', () => {
+  test('twice fires the event handler twice', () => {
+    const handleLike = jest.fn()
+
+    const component = render(
+      <Blog blog={blog} handleLike={handleLike} />
+    )
+
+    const showButton = component.getByText('view')
+    fireEvent.click(showButton)
+
+    const likeButton = component.getByText('like')
+    const times = 2
+    for (let i = 0; i < times; i++) {
+      fireEvent.click(likeButton)
+    }
+    expect(handleLike.mock.calls).toHaveLength(times)
   })
 })
