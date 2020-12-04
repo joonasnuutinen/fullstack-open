60cbb27a62446bd9ec339347983293aeb47c8540
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-12-04 13:13:34 +0200

    Do 6.14

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,3 @@
-const getId = () => (100000 * Math.random()).toFixed(0)
-
 const reducer = (state = [], action) => {
   //console.log('state now: ', state)
   //console.log('action', action)
@@ -17,14 +15,10 @@ const reducer = (state = [], action) => {
   }
 }
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
+    data
   }
 }
 
