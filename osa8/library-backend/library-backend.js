fcd02a7e09ccaef5c28fc9c54c7a51f5fe26b26e
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-12-11 08:44:57 +0200

    Do 8.6

--- osa8/library-backend/library-backend.js
+++ osa8/library-backend/library-backend.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -120,6 +130,22 @@ const resolvers = {
   },
   Author: {
     bookCount: root => books.filter(b => b.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      
+      if (authors.findIndex(a => a.name === book.author) === -1) {
+        const author = {
+          name: book.author,
+          id: uuid()
+        }
+        authors = authors.concat(author)
+      }
+
+      return book
+    }
   }
 }
 
