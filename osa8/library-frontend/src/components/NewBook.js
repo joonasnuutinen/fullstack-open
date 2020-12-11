8fe8543e0759c7942c7ba61fd5402a9a491ccbd5
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-12-11 10:33:11 +0200

    Do 8.10

--- osa8/library-frontend/src/components/NewBook.js
+++ osa8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,6 +8,10 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [createPerson] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+  })
 
   if (!props.show) {
     return null
@@ -15,6 +21,7 @@ const NewBook = (props) => {
     event.preventDefault()
     
     console.log('add book...')
+    createPerson({ variables: { title, author, published: Number(published), genres } })
 
     setTitle('')
     setPublished('')
