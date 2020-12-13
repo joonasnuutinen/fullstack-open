911acf7a24958b6d47220575eb1c6213922d034f
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-12-13 12:26:50 +0200

    Do 8.17

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -13,6 +13,7 @@ const Books = (props) => {
   if (result.loading) {
     return <Loading />
   }
+  console.log(result)
 
   const books = result.data.allBooks
 
@@ -34,7 +35,7 @@ const Books = (props) => {
           {books.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           )}
